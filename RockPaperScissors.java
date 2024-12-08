ff54e0602a363dc17051824abfabcf5c00ae149d
1y4nu <akanoim@gmail.com>
2024-12-07 20:43:10 -0400

    Added code to check for wrong user input

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 public class RockPaperScissors {
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Welcome to Rock Paper Scissors" + "\n" + 
-				"Choose your move(1, 2 or 3): " + "\n" +
-				"1. Rock" + "\n" +
-				"2. Paper" + "\n" +
-				"3. Scissors");
-		User user = new User(scan.nextInt());
+		int userIn = 0;
+		int process = 0;
+
+
+
+		System.out.println("Welcome to Rock Paper Scissors");
+
+		while (process < 1) {
+			System.out.println("Choose your move (1, 2 or 3): " + "\n" +
+					"1. Rock" + "\n" +
+					"2. Paper" + "\n" +
+					"3. Scissors");
+			userIn = scan.nextInt();
+			if (userIn >= 1 && userIn <= 3)
+				process++;
+			else
+				System.out.println("Invalid Input, please enter a number displayed\n");
+		}
+
+		User user = new User(userIn);
 		Opponent opponent = new Opponent();
 		String outcome = "";
 
@@ -19,20 +34,19 @@ public class RockPaperScissors {
 					outcome = "Rock beats Scissors, You win!";
 				else
 					outcome = "Paper beats Rock, You lose!";
+				}
+				else if (user.getMove().equals("Paper")) {
+					if (opponent.getMove().equals("Rock")) 
+						outcome = "Paper beats Rock, You win!";
+					else
+						outcome = "Scissors beats Paper, You lose!";
+				}
+				else if (user.getMove().equals("Scissors")) {
+					if (opponent.getMove().equals("Paper")) 
+						outcome = "Scissors beats Paper, You win!";
+					else
+						outcome = "Rock beats Scissors, You lose!";
+				}
 			}
-			else if (user.getMove().equals("Paper")) {
-				if (opponent.getMove().equals("Rock")) 
-					outcome = "Paper beats Rock, You win!";
-				else
-					outcome = "Scissors beats Paper, You lose!";
-			}
-			else if (user.getMove().equals("Scissors")) {
-				if (opponent.getMove().equals("Paper")) 
-					outcome = "Scissors beats Paper, You win!";
-				else
-					outcome = "Rock beats Scissors, You lose!";
-			}
-		}
 		System.out.println(outcome);
 	}
-}
